44c229acdc1a18cc7d741dcd5d9e090de0f1646c
I635230 <yugo.susuki@gmail.com>
2023-12-26 20:07:47 +0900

    :sparkles: Registrationのエンドポイントを実装 #1

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :email, presence: true
+  validates :username, presence: true
+  has_secure_password
+
+  def to_json(jwt_token = '')
+    { user: { email: email, 
+              token: jwt_token, 
+              username: username, 
+              bio: bio, 
+              image: image } }
+  end
+end
