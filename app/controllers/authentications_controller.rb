aa7529adde6a9cb57ce7f17baefc55019f58b25f
I635230 <yugo.susuki@gmail.com>
2023-12-26 20:33:39 +0900

    :sparkles: Authenticationのエンドポイントを実装

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,2 +1,13 @@
 class AuthenticationsController < ApplicationController
+  include JwtAuthenticator
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      jwt_token = encode({ user_id: @user.id })
+      render status: :created, json: @user.to_json(jwt_token)
+    else
+      render status: :unprocessable_entity
+    end
+  end
 end
