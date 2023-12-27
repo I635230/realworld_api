04b365f27578886aebcc3150f5f43c72b64856bc
I635230 <yugo.susuki@gmail.com>
2023-12-27 13:41:30 +0900

    :sparkles: Delete Articleのエンドポイントを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   scope :api do
     post "/users/login", to: "authentications#create"
     resources :users, only: %i[create]
-    resources :articles, param: :slug, only: %i[show create update]
+    resources :articles, param: :slug, only: %i[show create update destroy]
   end
 end
