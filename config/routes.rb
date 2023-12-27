8bb97b6696a3f5bd328e864a5aff44a74efe6394
I635230 <yugo.susuki@gmail.com>
2023-12-27 12:42:59 +0900

    :sparkles: Get Articleのエンドポイントを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   scope :api do
     post "/users/login", to: "authentications#create"
     resources :users, only: %i[create]
-    resources :articles, only: %i[create]
+    resources :articles, param: :slug, only: %i[create show]
   end
 end
