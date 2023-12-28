902a04c9466bd9c75fd3e19428c6dfcdb568f861
I635230 <yugo.susuki@gmail.com>
2023-12-28 19:07:33 +0900

    :sparkles: follow機能を実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,17 @@
 class ProfilesController < ApplicationController
 
-  before_action :authorized, only: %i[show]
+  before_action :authorized, only: %i[show follow]
 
   def show
     @current_user = User.find_by(id: @user_id)
     @some_user = User.find_by(username: params[:username])
+    render status: :ok, json: @some_user.to_profile(@current_user)
+  end
+
+  def follow
+    @current_user = User.find_by(id: @user_id)
+    @some_user = User.find_by(username: params[:username])
+    @current_user.follow(@some_user)
     render status: :ok, json: @some_user.to_profile(@current_user)
   end
 end
