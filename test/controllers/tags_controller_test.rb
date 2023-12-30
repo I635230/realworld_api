175db3fcb749cb85662503317cf3a9a5f6c2b154
I635230 <yugo.susuki@gmail.com>
2023-12-30 20:30:30 +0900

    :green_heart: Get Tagsのテスト fixes #63

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,4 +1,17 @@
 require "test_helper"
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:sakana)
+  end
+
+  test "認証なしでTagsを取得できる" do
+    get tags_path
+    assert_response :ok
+  end
+
+  test "認証ありでTagsを取得できる" do
+    get tags_path, headers: header_token(@user)
+    assert_response :ok
+  end
 end
