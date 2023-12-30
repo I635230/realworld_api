0ac94dda04afb5d2cf2e6cad9af8c00c15e94873
I635230 <yugo.susuki@gmail.com>
2023-12-30 15:39:53 +0900

    :green_heart: Get Current Userのテスト fixes #47

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def header_token(user)
+    { "Authorization": "Token #{ encode({ user_id: user.id })}" }
+  end
 end
