79d23902664de811987831ba974924b1e66fbd0f
I635230 <yugo.susuki@gmail.com>
2023-12-30 20:00:24 +0900

    :green_heart: Create Articleのテスト fixes #55

--- test/integration/articles_post_test.rb
+++ test/integration/articles_post_test.rb
@@ -5,15 +5,35 @@ class ArticlesPostTest < ActionDispatch::IntegrationTest
     @user = users(:sakana)
   end
 
+  test "認証なしでpostできない" do
+    assert_no_difference "Article.count" do
+      post articles_path, params: { article: { title: "title example",
+                                               description: "description example",
+                                               body: "body example",
+                                               tagList: ["example_tag_1", "example_tag_2"] } }
+    end
+    assert_response :unauthorized
+  end
+
+  test "invalidな情報でpostできない" do
+    assert_no_difference "Article.count" do
+      post articles_path, params: { article: { title: "",
+                                               description: "",
+                                               body: "" } }, 
+                                               headers: header_token(@user)
+    end
+    assert_response :unprocessable_entity
+  end
+
   test "validな情報でpostできる" do
     assert_difference "Article.count", 1 do
       post articles_path, params: { article: { title: "title example",
                                                description: "description example",
                                                body: "body example",
                                                tagList: ["example_tag_1", "example_tag_2"] } },
-                                               headers: { "Authorization": "Token #{encode({ user_id: @user.id })}" }
-      assert_response :created
+                                               headers: header_token(@user)
     end
+    assert_response :created
   end
 
   test "tagが空のときにpostできる" do
@@ -21,8 +41,8 @@ class ArticlesPostTest < ActionDispatch::IntegrationTest
       post articles_path, params: { article: { title: "title example",
                                                description: "description example",
                                                body: "body example" } },
-                                               headers: { "Authorization": "Token #{encode({ user_id: @user.id })}" }
-      assert_response :created
+                                               headers: header_token(@user)
     end
+    assert_response :created
   end
 end
